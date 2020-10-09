4f0d79ac70c81b7b2232a2706bcf9f1dc5fa59ce
satyadara <a.satya.syahputra@gmail.com>
2020-10-09 21:05:06 +0700

    Init

---
+++ README.md
@@ -0,0 +1,9 @@
+# README FIRST
+## Requirement
+1. Java
+2. Maven
+3. serve (node dependencies) (npm install -g serve)
+## Run Spring Boot
+`./mvnw spring-boot:run`
+## Run HTML
+`serve -s ./html`
--- src/main/java/com/satyadara/websocket/WebSocketController.java
+++ src/main/java/com/satyadara/websocket/WebSocketController.java
@@ -1,17 +1,19 @@
 package com.satyadara.websocket;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@Slf4j
 public class WebSocketController {
 
   @MessageMapping("/chat/{userId}")
   @SendTo("/topic/messages/{userId}")
   public OutputMessage send(@DestinationVariable String userId, final Message message) {
-    System.out.println(userId);
+    log.info(userId);
     return new OutputMessage(message.getFrom(), message.getText());
   }
 }
